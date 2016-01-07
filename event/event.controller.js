44e10a90c8ab591757e00f4cb837812b512dc8cc
Tyler Roberts <dnbxna@gmail.com>
2016-01-07 12:32:20 -0500

    login and local storage working. event.view.html mockup almost complete.  getting datetimepicker fn error

---
+++ event/event.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('EventController', EventController);
+
+    EventController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function EventController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.event = event;
+
+
+
+
+    }
+})();
+
+
+//todo:  datetimepicker is not a function
