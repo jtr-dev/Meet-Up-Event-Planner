5bc24a639a40907e974cdbeffde45b3b1410362b
Tyler Roberts <dnbxna@gmail.com>
2016-01-08 14:40:33 -0500

    datetime function fix

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Meet-Up-Event-Planner
+
+
+TODO:	
+	Password verification
+		Requirements
+			Length (At least 8 char)
+			At least 1 uppercase
+			At least 1 lowercase
+			At least 1 special character  !@#$%
+			
+			
+	Location Services to autofill 'Where' input
+	
+
+	
+				
--- event/event.controller.js
+++ event/event.controller.js
@@ -5,17 +5,36 @@
         .module('app')
         .controller('EventController', EventController);
 
-    EventController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function EventController($location, AuthenticationService, FlashService) {
+    EventController.$inject = ['$location', 'UserService', '$rootScope'];
+    function EventController($location, UserService, $rootScope) {
         var vm = this;
 
         vm.event = event;
+        vm.user = null;
+        vm.allUsers = [];
 
+        initController();
 
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
 
 
     }
 })();
 
-
-//todo:  datetimepicker is not a function
