5ade04f1ffbf0e161edd447a0d9c4faa9038ee2f
Tyler Roberts <dnbxna@gmail.com>
2016-01-15 17:13:26 -0500

    fixed regExp and password confirmation in register view  ( need to figure out why $scope is needed instead of vm

--- register/register.controller.js
+++ register/register.controller.js
@@ -1,16 +1,38 @@
 ﻿(function () {
     'use strict';
-
+ 
     angular
-        .module('app')
-        .controller('RegisterController', RegisterController);
-
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
+      .module('app')
+      .controller('RegisterController', RegisterController)
+      .directive('pwCheck', [function () {
+          return {
+              require: 'ngModel',
+              link: function (scope, elem, attrs, ctrl) {
+                  var firstPassword = '#' + attrs.pwCheck;
+                  elem.add(firstPassword).on('keyup', function () {
+                      scope.$apply(function () {
+                          // console.info(elem.val() === $(firstPassword).val());
+                          ctrl.$setValidity('pwmatch', elem.val() === $(firstPassword).val());
+                      });
+                  });
+              }
+          }
+      }]);
+ 
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService', '$scope'];
+    function RegisterController(UserService, $location, $rootScope, FlashService, $scope) {
         var vm = this;
+        $scope.myRegex = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+        //vm.myRegex = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+        
+        vm.password = '';
+        
+
+        //vm.myRegex = new RegExp("/[a-zA-Z]{4}[0-9]{6,6}[a-zA-Z0-9]{3}/");
+        ////vm.myRegex = /[a-zA-Z]{4}[0-9]{6,6}[a-zA-Z0-9]{3}/;
 
         vm.register = register;
-
+ 
         function register() {
             vm.dataLoading = true;
             UserService.Create(vm.user)
@@ -26,4 +48,7 @@
         }
     }
 
+
+
+ 
 })();
